ac9f17889c7832fca4a52e3f600eb085261f7374
Anmol Agarwal <mailanmolat@gmail.com>
2022-07-08 03:02:54 +0530

    Implemented Remove account

--- script.js
+++ script.js
@@ -164,6 +164,16 @@ btnTransfer.addEventListener('click', function (e) {
 
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('Delete');
+  if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
